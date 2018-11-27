bff3b2a3ec2687db430d694995ffc2348426db2e
Dmitry Baginsky <dmitri.baginski@gmail.com>
2018-11-27 18:48:36 +0300

    [Hyper] Update config

--- .hyper.js
+++ .hyper.js
@@ -71,7 +71,7 @@ module.exports = {
     showWindowControls: false,
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '0px 3px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -145,7 +145,8 @@ module.exports = {
     "hyper-solarized-dark",
     "hyper-pane",
     "hyperterm-summon",
-    "hyper-tabs-enhanced"
+    "hyper-tabs-enhanced",
+    "hypercwd"
   ],
 
   // in development, you can create a directory under
