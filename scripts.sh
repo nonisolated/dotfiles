a7276588e6808122bdd40412d2dd55fb5cbc8d2c
Dmitry Baginsky <dmitri.baginski@gmail.com>
2018-12-03 11:08:58 +0300

    [Scripts] Fix Xcode launching

--- scripts.sh
+++ scripts.sh
@@ -16,7 +16,7 @@ function open_applications() {
     "home")
         declare -a apps=("Safari" "Spark" "Telegram" "Mate Translate") ;;
     "work")
-        declare -a apps=("Safari" "Spark" "Telegram" "Skype" "Slack" "Xcode" "Paw" "Dropbox" "Mate Translate") ;;
+        declare -a apps=("Safari" "Spark" "Telegram" "Skype" "Slack" "/Applications/Xcode.app" "Paw" "Dropbox" "Mate Translate") ;;
 	esac
 
 	for app in "${apps[@]}"; do
